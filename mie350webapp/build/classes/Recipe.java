751d4fb3c864254fcc3a4b2fe7969fd3f9b81a63
emilyjiao <45907366+emilyjiao@users.noreply.github.com>
2021-11-12 21:09:48 -0500

    Added Recipe class, unsure if ingredients should be an array of string or not

---
+++ mie350webapp/build/classes/Recipe.java
@@ -0,0 +1,14 @@
+class Recipe {
+	private int postID;
+	private String recipeTitle;
+	private String imageUrl;
+	private int serving;
+	private String difficulty;
+	private double prepTime;
+	private String[] ingredients;
+	private String instructions;
+	private String dishType;
+	private String cuisineType;
+	private String username;
+	
+}
