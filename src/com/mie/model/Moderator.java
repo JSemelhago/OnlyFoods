b62b8dfa11d708fa8b92ef226d59e2ee8e147222
emilyjiao <45907366+emilyjiao@users.noreply.github.com>
2021-11-29 16:37:07 -0500

    update moderator query

--- src/com/mie/model/Moderator.java
+++ src/com/mie/model/Moderator.java
@@ -2,8 +2,33 @@ package com.mie.model;
 
 public class Moderator extends User{
 	//admin constructor
-		public Moderator(String username, String password) {
-			super(username,password);
+	private ArrayList<String> flaggedStudents;
+	public Moderator(String username, String password) {
+		super(username,password);
+	}
+	public Moderator() {
+		
+	}
+	public ArrayList<String> getFlaggedStudents() {
+		return flaggedStudents;
+	}
+	public void printFlaggedStudents() {
+		for (int i = 0; i < flaggedStudents.length; i++) {
+			print(flaggedStudents.get(i));
 		}
+	}
+	public void addFlaggedStudents(String flagStu) {
+		flaggedStudents.add(flagStu);
+	}
+	public void updateModerator() {
+		String flgStr = "";
+		for (int i = 0; i < flaggedStudents.length-1; i++) {
+			flgStr += flaggedStudents.get(i)+",";
+		}
+		flgStr += flaggedStudents.get(flaggedStudents.length-1);
+		String qStr = "UPDATE Moderator Set Password ='" + password + "',  FlaggedStudents='" + flgstr + "' WHERE Username =" + username + "; ";
+				OnlyFoodsRunner.runUpdate(qStr);
+	}
+	
 		
 }
